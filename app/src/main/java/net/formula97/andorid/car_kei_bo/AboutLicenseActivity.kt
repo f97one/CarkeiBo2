0708bdda306fb07febae2a275ce8c8a649479d50
HAJIME Fukuna <f97one@gmail.com>
2018-08-13 10:14:06 +0900

    ActivityひとつをJavaからKotlinに変換したので、各種Kotlinサポートを追加。

---
+++ app/src/main/java/net/formula97/andorid/car_kei_bo/AboutLicenseActivity.kt
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package net.formula97.andorid.car_kei_bo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author kazutoshi
+ */
+class AboutLicenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // TODO 自動生成されたメソッド・スタブ
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.aboutlicense)
+    }
+
+}
