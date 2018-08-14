3a57c5e0527272bdfc9810b58d778b47fc1bafa8
HAJIME Fukuna <f97one@gmail.com>
2018-08-14 18:54:28 +0900

    とりあえずDataSourceとなるinterfaceを追加。 refs #29 setProgress30 #29

---
+++ app/src/main/java/net/formula97/andorid/car_kei_bo/repository/DataSource.kt
@@ -0,0 +1,40 @@
+package net.formula97.andorid.car_kei_bo.repository
+
+/**
+ * 基礎的なデータアクセス処理を規定するinterface。
+ *
+ * T : Entityの型
+ *
+ * P : プライマリキーの型
+ */
+interface DataSource<T, P> {
+    /**
+     * Entityをinsertする。
+     */
+    fun addItem(entity : T)
+
+    /**
+     * EntityのListをまとめてinsertする。
+     */
+    fun addItems(entities : List<T>)
+
+    /**
+     * Entityをupdateする。
+     */
+    fun updateItem(entity: T)
+
+    /**
+     * Entityのリストをまとめてupdateする。
+     */
+    fun updateItems(entities: List<T>)
+
+    /**
+     * Entityをdeleteする。
+     */
+    fun removeItem(entity: T)
+
+    /**
+     * プライマリキーでEntityを探す。
+     */
+    fun findById(id: P)
+}
