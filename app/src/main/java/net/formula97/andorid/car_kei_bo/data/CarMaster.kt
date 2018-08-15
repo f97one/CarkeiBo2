8f2f2e886d70e41c899a8f62cb13b9c94bad3e2e
HAJIME Fukuna <f97one@gmail.com>
2018-08-15 19:37:02 +0900

    現状の CAR_MASTER の構造に一致していなかった件を修正。 refs #29, #28

--- app/src/main/java/net/formula97/andorid/car_kei_bo/data/CarMaster.kt
+++ app/src/main/java/net/formula97/andorid/car_kei_bo/data/CarMaster.kt
@@ -10,5 +10,21 @@ data class CarMaster(
         @ColumnInfo(name = "CAR_ID")
         var carId: Long,
         @ColumnInfo(name = "CAR_NAME")
-        var carName: String
+        var carName: String,
+        @ColumnInfo(name = "DEFAULT_FLAG")
+        var defaultFlag: Boolean,
+        @ColumnInfo(name = "CURRENT_FUEL_MILEAGE")
+        var currentFuelMileage: Double,
+        @ColumnInfo(name = "CURRENT_RUNNING_COST")
+        var currentRunningCost: Double,
+        @ColumnInfo(name = "PRICEUNIT")
+        var priceUnit: String,
+        @ColumnInfo(name = "DISTANCEUNIT")
+        var distanceUnit: String,
+        @ColumnInfo(name = "VOLUMEUNIT")
+        var volumeUnit: String,
+        @ColumnInfo(name = "FUELMILEAGE_LABEL")
+        var fuelMileageLabel: String,
+        @ColumnInfo(name = "RUNNINGCOST_LABEL")
+        var runningCostLabel: String
 )
