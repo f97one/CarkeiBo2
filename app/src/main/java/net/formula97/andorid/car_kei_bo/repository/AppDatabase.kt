b10c4871c7a6be7d5b3d2a19d7f057fcab7d6212
HAJIME Fukuna <f97one@gmail.com>
2018-08-18 20:12:13 +0900

    処理の分散を防止するため、マイグレーション管理をAppDatabaseに移動。 refs #37 setProgress10 #37

--- app/src/main/java/net/formula97/andorid/car_kei_bo/repository/AppDatabase.kt
+++ app/src/main/java/net/formula97/andorid/car_kei_bo/repository/AppDatabase.kt
@@ -1,7 +1,9 @@
 package net.formula97.andorid.car_kei_bo.repository
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.migration.Migration
 
 import net.formula97.andorid.car_kei_bo.data.CarMaster
 import net.formula97.andorid.car_kei_bo.data.CarMasterDao
@@ -10,7 +12,7 @@ import net.formula97.andorid.car_kei_bo.data.CostsMasterDao
 import net.formula97.andorid.car_kei_bo.data.LubMaster
 import net.formula97.andorid.car_kei_bo.data.LubMasterDao
 
-@Database(entities = arrayOf(CarMaster::class, CostsMaster::class, LubMaster::class),
+@Database(entities = [CarMaster::class, CostsMaster::class, LubMaster::class],
         version = AppDatabase.CURRENT_DATABASE_VERSION)
 abstract class AppDatabase : RoomDatabase() {
 
@@ -23,5 +25,17 @@ abstract class AppDatabase : RoomDatabase() {
     companion object {
         const val CURRENT_DATABASE_VERSION = 2
         const val DATABASE_FILENAME = "fuel_mileage.db"
+
+        /**
+         * 1 → 2 のマイグレーション
+         *
+         * マイグレーション管理テーブルを作るだけなので、特に何もしない。
+         */
+        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+
+            }
+        }
+
     }
 }
