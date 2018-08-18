06c7ccd2dc7cb82e57af0c6257d052229a7103a8
HAJIME Fukuna <f97one@gmail.com>
2018-08-18 20:42:56 +0900

    データアクセスがからむビジネスロジックまわりの処理を格納するクラスを用意。 refs #32 setProgress10 #32

---
+++ app/src/main/java/net/formula97/andorid/car_kei_bo/logic/BaseAppLogic.kt
@@ -0,0 +1,5 @@
+package net.formula97.andorid.car_kei_bo.logic
+
+import net.formula97.andorid.car_kei_bo.repository.AppDatabase
+
+open class BaseAppLogic(protected val appDatabase: AppDatabase)
---
+++ app/src/main/java/net/formula97/andorid/car_kei_bo/logic/CarListLogic.kt
@@ -0,0 +1,6 @@
+package net.formula97.andorid.car_kei_bo.logic
+
+import net.formula97.andorid.car_kei_bo.repository.AppDatabase
+
+class CarListLogic(appDatabase: AppDatabase) : BaseAppLogic(appDatabase) {
+}
