e82b86e6659589751d3331ce5a0014f446656a80
HAJIME Fukuna <f97one@gmail.com>
2018-09-06 21:23:46 +0900

    汎用メッセージ表示用DialogFragmentを仮実装。 refs #43 setProgress70 #43

---
+++ app/src/main/java/net/formula97/andorid/car_kei_bo/activity/MsgDialog.kt
@@ -0,0 +1,89 @@
+package net.formula97.andorid.car_kei_bo.activity
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import java.util.*
+
+class MsgDialog : DialogFragment() {
+
+    /**
+     * このダイアログのボタンを押したときの処理
+     */
+    interface OnDialogButtonClickCallback : EventListener {
+        /**
+         * ポジティブボタンを押したときの処理
+         *
+         * @param msgResId 表示しているメッセージのResId
+         */
+        fun onPositiveClick(msgResId: Int)
+
+        /**
+         * ネガティブボタンを押したときの処理
+         *
+         * @param msgResId 表示しているメッセージのResId
+         */
+        fun onNegativeClick(msgResId: Int)
+
+        /**
+         * 中ボタンを押したときの処理
+         *
+         * @param msgResId 表示しているメッセージのResId
+         */
+        fun onMiddleClick(msgResId: Int)
+    }
+
+    companion object {
+        /**
+         * このDialogFragmentを探すときのタグ
+         */
+        const val DIALOG_TAG = "net.formula97.andorid.car_kei_bo.activity.MsgDialog.DIALOG_TAG"
+
+        private val conditionBundleTag = "conditionBundleTag"
+
+        private lateinit var btnCallback: OnDialogButtonClickCallback
+
+        fun getInstance(condition: MsgDialogCondition, callback: OnDialogButtonClickCallback): MsgDialog {
+            btnCallback = callback
+
+            val dialog = MsgDialog()
+
+            val bundle: Bundle = Bundle()
+            bundle.putSerializable(conditionBundleTag, condition)
+            dialog.arguments = bundle
+
+            dialog.isCancelable = condition.cancelable
+
+            return dialog
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder : AlertDialog.Builder = AlertDialog.Builder(activity)
+
+        val args = arguments!!.getSerializable(conditionBundleTag) as MsgDialogCondition
+
+        // アイコン
+        if (args.iconResId != Int.MIN_VALUE) {
+            builder.setIcon(args.iconResId)
+        }
+        // タイトル
+        if (args.titleResId != Int.MIN_VALUE) {
+            builder.setTitle(args.titleResId)
+        }
+
+        builder.setMessage(args.messageResId)
+
+        // ボタンの生成
+        builder.setPositiveButton(args.positiveButtonMsgResId) { dialog, which -> btnCallback.onPositiveClick(args.messageResId) }
+        if (args.needNegativeButton) {
+            builder.setNegativeButton(args.negativeButtonMsgResId) { dialog, which -> btnCallback.onNegativeClick(args.messageResId) }
+        }
+        if (args.needMiddleButton) {
+            builder.setNeutralButton(args.middleButtonMsgResId) { dialog, which -> btnCallback.onMiddleClick(args.messageResId) }
+        }
+
+        return builder.create()
+    }
+}
---
+++ app/src/main/java/net/formula97/andorid/car_kei_bo/activity/MsgDialogCondition.kt
@@ -0,0 +1,41 @@
+package net.formula97.andorid.car_kei_bo.activity
+
+import java.io.Serializable
+
+/**
+ * メッセージダイアログ表示コンディションのDTO。
+ */
+data class MsgDialogCondition(var messageResId: Int) : Serializable {
+    /**
+     * ダイアログタイトルのResId
+     */
+    var titleResId: Int = Int.MIN_VALUE
+    /**
+     * アイコンのResId
+     */
+    var iconResId: Int = Int.MIN_VALUE
+    /**
+     * ネガティブボタン作成要否
+     */
+    var needNegativeButton: Boolean = false
+    /**
+     * 中ボタン作成要否
+     */
+    var needMiddleButton: Boolean = false
+    /**
+     * ポジティブボタン表示名
+     */
+    var positiveButtonMsgResId: Int = android.R.string.ok
+    /**
+     * ネガティブボタン表示名
+     */
+    var negativeButtonMsgResId: Int = android.R.string.no
+    /**
+     * 中ボタン表示名
+     */
+    var middleButtonMsgResId: Int = android.R.string.cancel
+    /**
+     * ダイアログを Back でキャンセル可能か
+     */
+    var cancelable: Boolean = true
+}
